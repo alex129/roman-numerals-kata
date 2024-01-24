0f6185ae6dbe5cac38be87377ae40a5cacf8cf8d
Plk <plikforo@gmail.com>
2024-01-24 16:28:49 +0100

    test: :test_tube: should convert 2 to II

--- src/ArabicToRomanConverter.ts
+++ src/ArabicToRomanConverter.ts
@@ -1,5 +1,9 @@
 export class ArabicToRomanConverter {
     static convert(arabicNumber: number) {
+        if(arabicNumber === 2) {
+            return "II";
+        }
+        
         return "I";
     }
 }
--- src/test/ArabicToRomanConverter.test.ts
+++ src/test/ArabicToRomanConverter.test.ts
@@ -4,4 +4,8 @@ describe("Arabic to roman converter", () => {
     test("should convert 1 to I", () => {
         expect(ArabicToRomanConverter.convert(1)).toBe("I");
     });
+
+    test("should convert 2 to II", () => {
+        expect(ArabicToRomanConverter.convert(2)).toBe("II");
+    });
 });
