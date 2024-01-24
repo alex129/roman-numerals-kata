ab0bc929e67bfa5f8b8731f528d9539b9c38a1f2
Plk <plikforo@gmail.com>
2024-01-24 16:38:51 +0100

    refactor: :recycle: Refactor arabic roman converter with ArabicRomanDictionary

--- src/ArabicToRomanConverter.ts
+++ src/ArabicToRomanConverter.ts
@@ -1,11 +1,7 @@
+import { ArabicRomanDictionary } from "./constants";
+
 export class ArabicToRomanConverter {
     static convert(arabicNumber: number) {
-        if(arabicNumber === 2) {
-            return "II";
-        } else if(arabicNumber === 3) { 
-            return "III";
-        }
-
-        return "I";
+        return ArabicRomanDictionary[arabicNumber];
     }
 }
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const ArabicRomanDictionary: Record<number, string> = {
+    1: "I",
+    2: "II",
+    3: "III",
+}
