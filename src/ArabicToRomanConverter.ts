9afa43a687bc754bf03dc74f62bf2ca1c0666eea
Plk <plikforo@gmail.com>
2024-01-25 09:53:54 +0100

    test: :test_tube: should convert 11 to XI

--- src/ArabicToRomanConverter.ts
+++ src/ArabicToRomanConverter.ts
@@ -1,7 +1,15 @@
 import { ArabicRomanDictionary } from "./constants";
 
 export class ArabicToRomanConverter {
-    static convert(arabicNumber: number) {
-        return ArabicRomanDictionary[arabicNumber];
+  static convert(arabicNumber: number) {
+    const romanNumber = ArabicRomanDictionary[arabicNumber];
+
+    if(!romanNumber) {
+      const integerPart = Math.floor(arabicNumber / 10);
+      const remainder = arabicNumber % 10;
+
+      return ArabicRomanDictionary[integerPart * 10] + ArabicRomanDictionary[remainder];
     }
+    return romanNumber;
+  }
 }
--- src/test/ArabicToRomanConverter.test.ts
+++ src/test/ArabicToRomanConverter.test.ts
@@ -1,15 +1,23 @@
 import { ArabicToRomanConverter } from "../ArabicToRomanConverter";
 
 describe("Arabic to roman converter", () => {
-    test("should convert 1 to I", () => {
-        expect(ArabicToRomanConverter.convert(1)).toBe("I");
-    });
+  test("should convert 1 to I", () => {
+    expect(ArabicToRomanConverter.convert(1)).toBe("I");
+  });
 
-    test("should convert 2 to II", () => {
-        expect(ArabicToRomanConverter.convert(2)).toBe("II");
-    });
+  test("should convert 2 to II", () => {
+    expect(ArabicToRomanConverter.convert(2)).toBe("II");
+  });
 
-    test("should convert 3 to III", () => {
-        expect(ArabicToRomanConverter.convert(3)).toBe("III");
-    });
+  test("should convert 3 to III", () => {
+    expect(ArabicToRomanConverter.convert(3)).toBe("III");
+  });
+
+  test("should convert 4 to IV", () => {
+    expect(ArabicToRomanConverter.convert(4)).toBe("IV");
+  });
+
+  test("should convert 11 to XI", () => {
+    expect(ArabicToRomanConverter.convert(11)).toBe("XI");
+  });
 });
